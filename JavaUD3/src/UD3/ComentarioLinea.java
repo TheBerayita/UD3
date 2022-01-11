70c03fda4a490dcc5c2fd2872bf531c997b61f2a
TheBerayita <97508463+TheBerayita@users.noreply.github.com>
2022-01-11 10:06:36 +0100

    UD3

---
+++ JavaUD3/src/UD3/ComentarioLinea.java
@@ -0,0 +1,9 @@
+package UD3;
+
+public class ComentarioLinea {
+	public static void main(String args[]) {
+		//System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DEL PROGRAMA");
+	}
+}
